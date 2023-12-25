ccec35d8aa8182855c21ea4b725e3f919ab347c4
delabahmani <delabahmani@gmail.com>
2023-12-25 16:24:46 -0500

    added menu list

---
+++ src/components/MenuList.jsx
@@ -0,0 +1,177 @@
+const MenuList = [
+  {
+    id: 1,
+    name: 'Golden Caviar Blinis',
+    type: 'Appetizer',
+    img: '../assets/caviar.png',
+    description: 'Delicate mini pancakes topped with premium beluga caviar and a hint of crème fraîche.'
+  },
+  {
+    id: 2,
+    name:  'Truffle Carpaccio',
+    type: 'Appetizer',
+    img: '../assets/truffle.png',
+    description: 'Thinly sliced black truffles, served with microgreens and a light citrus vinaigrette.'
+  },
+  {
+    id: 3,
+    name:  'Fois Gras Torchon',
+    type: 'Appetizer',
+    img: '../assets/fois-gras.png',
+    description: 'Luxurious goose liver pâté, accompanied by fig compote and toasted brioche.'
+  },
+  {
+    id: 4,
+    name:  'Lobster Tartare',
+    type: 'Appetizer',
+    img: '../assets/lobster-tartare.png',
+    description: 'Finely chopped lobster meat, seasoned with herbs and served with a saffron aioli.'
+  },
+  {
+    id: 5,
+    name:  'Wagyu Beef Carpaccio',
+    type: 'Appetizer',
+    img: '../assets/wagyu.png',
+    description: 'Paper-thin slices of marbled wagyu beef, drizzled with truffle oil and parmesan shavings.'
+  },
+  {
+    id: 6,
+    name:  'Saffron-Infused Scallops',
+    type: 'Appetizer',
+    img: '../assets/scallops.png',
+    description: 'Seared scallops on a bed of saffron risotto, garnished with gold leaf.'
+  },
+  {
+    id: 7,
+    name:  'Butter-Poached Lobster Tail',
+    type: 'Entreé',
+    img: '../assets/poached-lobster.png',
+    description: 'Served with a white wine reduction and caviar garnish.'
+  },
+  {
+    id: 8,
+    name:  'Wagyu Beef Filet Mignon',
+    type: 'Entreé',
+    img: '../assets/filet-mignon.png',
+    description: 'Cooked to perfection, accompanied by a rich truffle sauce.'
+  },
+  {
+    id: 9,
+    name:  'Seared Bluefin Tuna Steak',
+    type: 'Entreé',
+    img: '../assets/bluefin-tuna.png',
+    description: 'With a soy-ginger glaze and pickled radish.'
+  },  
+  {
+    id: 10,
+    name:  'King Crab Risotto',
+    type: 'Entreé',
+    img: '../assets/risotto.png',
+    description: 'Creamy risotto with chunks of Alaskan king crab and a saffron infusion.'
+  },  
+  {
+    id: 11,
+    name:  'Rack of Lamb',
+    type: 'Entreé',
+    img: '../assets/lamb-rack.png',
+    description: 'Herb-crusted, served with a mint-infused demi-glace.'
+  },  
+  {
+    id: 12,
+    name:  "Duck Breast à l'Orange",
+    type: 'Entreé',
+    img: '../assets/duck-breast.png',
+    description: 'Pan-seared duck breast with a classic orange sauce.'
+  },  
+  {
+    id: 13,
+    name:  'Veal Chop Milanese',
+    type: 'Entreé',
+    img: '../assets/veal-chop.png',
+    description: 'Breaded and fried, topped with arugula and cherry tomatoes.'
+  },  
+  {
+    id: 14,
+    name:  'Chilean Sea Bass',
+    type: 'Entreé',
+    img: '../assets/sea-bass.png',
+    description: 'Grilled, served with a lemon-butter caper sauce.'
+  },  
+  {
+    id: 15,
+    name:  'Venison Loin',
+    type: 'Entreé',
+    img: '../assets/venison.png',
+    description: 'Grilled, served with a berry reduction and root vegetables.'
+  },  
+  {
+    id: 16,
+    name:  'Grilled Octopus',
+    type: 'Entreé',
+    img: '../assets/octopus.png',
+    description: 'Tender octopus with a chorizo vinaigrette.'
+  },  
+  {
+    id: 17,
+    name:  'Chocolate Soufflé',
+    type: 'Dessert',
+    img: '../assets/souffle.png',
+    description: 'Served with a gold-dusted raspberry coulis.'
+  },
+  {
+    id: 18,
+    name:  'Vanilla Bean Panna Cotta',
+    type: 'Dessert',
+    img: '../assets/panna-cotta.png',
+    description: 'With a berry compote and edible flowers.'
+  },
+  {
+    id: 19,
+    name:  'Lemon Tart',
+    type: 'Dessert',
+    img: '../assets/lemon-tart.png',
+    description: 'With a brûléed meringue topping.'
+  },
+  {
+    id: 20,
+    name:  'Water',
+    type: 'Drinks',
+    img: '../assets/water.png',
+    description: 'Sparkling or still mineral water.'
+  },
+  {
+    id: 21,
+    name:  'Soda',
+    type: 'Drinks',
+    img: '../assets/soda.png',
+    description: 'A selection of artisanal, crafted sodas.'
+  },
+  {
+    id: 22,
+    name:  'Beer/Wine',
+    type: 'Drinks',
+    img: '../assets/beer-wine.png',
+    description: 'Premium craft beers and a selection of fine wines.'
+  },
+  {
+    id: 23,
+    name:  'Cocktail',
+    type: 'Drinks',
+    img: '../assets/cocktail.png',
+    description: 'Signature cocktails crafted by our mixologist.'
+  },
+  {
+    id: 24,
+    name:  'Champagne',
+    type: 'Drinks',
+    img: '../assets/champagne.png',
+    description: 'A glass of vintage champagne.'
+  },
+  // {
+  //   id: ,
+  //   name:  '',
+  //   type: '',
+  //   img: '../assets/',
+  //   description: ''
+  // },
+]
